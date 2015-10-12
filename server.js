6089b18d2d844daaeb2745a6d25aae6ab997e8ed
viorel <viorel.topa@gmail.com>
2015-10-12 21:38:53 +0000

    al nustiu catalea

--- server.js
+++ server.js
@@ -65,23 +65,22 @@ function download( file, resp ){
 
 function downPDF( body, resp ){
 	
-	//var pdf = require('html-pdf');
+	var pdf = require('html-pdf');
 	//resp.end(body.substr(0,200) + body.slice(-200));
 	//return;
 	
 	var cnt = JSON.parse(body);
-	//resp.writeHead(200, {'Content-disposition': 'attachment; filename='+ cnt.fileName + '.pdf'});
+	resp.writeHead(200, {'Content-disposition': 'attachment; filename='+ cnt.fileName + '.pdf'});
 	
-	resp.end(cnt.fileName + '\n' + cnt.borders + '\n' + cnt.footer + '\n' + cnt.body);
-	return;
+	//resp.end(cnt.fileName + '\n' + cnt.borders + '\n' + cnt.footer + '\n' + cnt.body);
+	//return;
 	
-	/*var html = '<HTML><BODY>' + cnt.content + '</BODY></HTML>';
+	var html = '<HTML><BODY>' + cnt.content + '</BODY></HTML>';
 	
-	/var opts = {};
+	var opts = {};
 	
 	var pdf = require('html-pdf');
 	pdf.create(html, opts).toStream(function(err, stream){
 	  	stream.pipe(resp);
 	});
-*/
 }
