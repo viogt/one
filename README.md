f59db2eec89a5587fa0f264672a2d850dcf32dd9
viorel <viorel.topa@gmail.com>
2015-10-25 11:34:46 +0000

    Versiunea cu newPDF

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ http://openshift.github.io/documentation/oo_cartridge_guide.html#nodejs
 > git remote add <app> -m master ssh://5468e9e2e0b8cd1a5800000d@the-postviorel.rhcloud.com/~/git/the.git/
 > git pull -s recursive -X theirs <app> master
 > git push --set-upstream <app> master
+
+---
+
+remote access
+
+> rhc ssh <app_name>
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http	= require('http'),
     fs		= require('fs'),
     path = require('path'),
+    pdf = require('html-pdf'),
     ipadd	= process.env.OPENSHIFT_NODEJS_IP,
     port	= process.env.OPENSHIFT_NODEJS_PORT || 8080,
     body	= '';
@@ -16,16 +17,19 @@ http.createServer(function (req, res) {
     	else res.end('Error: unknown request!');
     	return;
   	}
-	if(req.url.substr(0,10)=='/files/pdf') {
+
+  if(req.url.substr(0,12)=='/files/pipe/') {
 		body = '';
 		req.on('data', function (chunk) { body += chunk; });
-		req.on('end', function () { downPDF(body, res); });
+		req.on('end', function () { pipping(req.url.substr(12), body, res); });
   	}
+    
 	else if(req.url.substr(0,7)=='/files/') {
 		body = '';
 		req.on('data', function (chunk) { body += chunk; });
 		req.on('end', function () { saveFile('.' + req.url, body, res); });
   	}
+
 	else res.end('Error: unknown request! (' + req.url + ')');
   
 }).listen(port, ipadd);
@@ -63,25 +67,48 @@ function download( file, resp ){
 	filestream.pipe(resp);
 }
 
-function downPDF( body, resp ){
-	
-	var pdf = require('html-pdf');
-	var cnt = JSON.parse(body);
-	resp.end( cnt.fileName + '\n' + cnt.borders + '\n' + cnt.footer + '\n' + cnt.body );
-	return;
-	
-	/*
-	resp.writeHead(200, {'Content-disposition': 'attachment; filename='+ cnt.fileName + '.pdf'});
-	
-	//resp.end(cnt.fileName + '\n' + cnt.borders + '\n' + cnt.footer + '\n' + cnt.body);
-	//return;
-	
-	var html = '<HTML><BODY>' + cnt.content + '</BODY></HTML>';
-	
-	var opts = {};
-	
-	var pdf = require('html-pdf');
+function pipping( file, body, resp ){
+
+  console.log('File = ' + file);
+  var html, wh = body.charAt(0);
+	var cnt = JSON.parse(body.substr(2));
+  console.log( 'FileName = ' + cnt.fileName );
+  
+  if(wh == 'S') html = cnt.content;
+  else html = '<HTML><BODY style="'+ cnt.body +'">'+cnt.content+'</BODY></HTML>';
+
+    	var opts = {
+      "format": 'A4',
+      "footer": {
+        "height": cnt.borders.footer,
+        "contents": cnt.footer
+      },
+      "orientation": "portrait",
+      "border": {
+        "top": cnt.borders.top,
+        "right": cnt.borders.right,
+        "bottom": cnt.borders.bottom,
+        "left": cnt.borders.left
+      },
+      "quality": "100"
+    };
+
+    if(cnt.bkgr) opts.border = '0px';
+  
+  if(wh=='Q' || wh=='S') {
+    resp.writeHead(200,{'Content-disposition': 'inline; filename='+cnt.fileName});
+    resp.writeHead(200,{'Content-type':'application/pdf'});
+  }
+  else resp.writeHead(200, {'Content-disposition': 'attachment; filename='+cnt.fileName});
+  
 	pdf.create(html, opts).toStream(function(err, stream){
 	  	stream.pipe(resp);
-	});*/
+	});
 }
+
+/*
+pdf.create(html, opts).toFile('./files/' +fileName, function(err, res) {
+  if (err) return console.log(err);
+  console.log('PDF created.');
+	resp.end( fileName );
+*/
