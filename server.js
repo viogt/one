4e606ee393a144cc2452053d74fc436e66862b86
viorel <viorel.topa@gmail.com>
2017-06-24 07:54:50 +0000

    ajax modificat

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ http.createServer(function (req, res) {
 
 	if(req.method == 'GET') {
 		if(req.url == '/') returnFile('./index.html', res);
+		else if(req.url.substr(0,11)=='/files/mng/') {
+		    operate( JSON.parse( decodeURIComponent( req.url.substr(11) ) ), res );
+		}
     	else if(req.url.substr(0,12)=='/files/down/') download('./files/'+req.url.substr(12) ,res);
     	else if(req.url.substr(0,7)=='/files/') returnFile('.' + req.url, res);
     	else res.end('Error: unknown request!');
